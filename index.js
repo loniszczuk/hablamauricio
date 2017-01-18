7cf2781f05422d71576d20f16591fe87f69ee8cb
Leandro Oniszczuk <loniszczuk@medallia.com>
2017-01-17 19:33:51 -0800

    first vesion

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,12 +10,17 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
-  response.render('pages/index');
+    fs.readFile('phrases.txt', 'utf8', function (err,data) {
+        var phrases = data.split('\n').filter(str => str.trim());
+        var random_line = Math.floor(Math.random() * phrases.length);
+        response.render('pages/index', {
+            phrase: phrases[random_line]
+        });
+    });
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
