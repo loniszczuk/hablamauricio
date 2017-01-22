8ad20842d41c1573d9e72eeb98eabd776cc87e4d
Leandro Oniszczuk <loniszczuk@medallia.com>
2017-01-21 19:33:31 -0800

    single page, deep links

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,25 @@
-angular.module("mauricioApp", ['ngRoute'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when("/", {
-        templateUrl: "joke.html",
-        controller: "JokesController",
-        resolve: {
-          jokes: function(Jokes) {
-              return Jokes.getJokes();
-          }
+(function(){
+    var app = angular.module("mauricioApp", ['ngRoute']);
+
+    app.controller('JokesController', ['$http', '$scope', '$location', function($http, $scope, $location) {
+        var ctrl = this;
+        $scope.jokes = [];
+        $scope.currentJoke = null;
+
+        $scope.newJoke = function() {
+            $scope.currentJoke = $scope.jokes[Math.floor(Math.random()*$scope.jokes.length)];
+            $location.hash($scope.currentJoke.id);
         }
-      })
-  })
-  .service("Jokes", function($http) {
-    this.getJokes = function() {
-      return $http.get("/api/jokes").
-        then(function(response) {
-            return response;
-        }, function(response) {
-            alert("Error retrieving jokes.");
+
+        $http.get("/api/jokes").success( function(response) {
+            $scope.jokes = response;
+            var hash = $location.hash();
+            if (hash) {
+                $scope.currentJoke = $scope.jokes.find( j => j.id === hash);
+            } else {
+                $scope.newJoke();
+            }
+
         });
-    }
-  })
-  .controller("JokesController", function(jokes, $scope) {
-    var randomJoke = jokes.data[Math.floor(Math.random()*jokes.data.length)];
-    $scope.joke = randomJoke;
-  });
+    }]);
+})();
